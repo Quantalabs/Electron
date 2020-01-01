a50bb51a55306ff024d66ed45fa8501ff6c153a5
Quant <55121845+Quantalabs@users.noreply.github.com>
2019-12-31 18:07:44 -0800

    Update v0.1 Rose.js

--- v0.1 Rose.js
+++ v0.1 Rose.js
@@ -1,1 +1,25 @@
-
+var line = function (x1, y1, x2, y2, cv) {
+  cv.beginPath();
+  cv.moveTo(x1,y1);
+  cv.lineTo(x2,y2);
+  cv.stroke();
+};
+var fillTri = function (x1, y1, x2, y2, x3, y3, cv) {
+  cv.beginPath();
+  cv.moveTo(x1,y1);
+  cv.lineTo(x2,y2);
+  cv.lineTo(x3,y3);
+  cv.lineTo(x1,y1);
+  cv.fill();
+};
+var strokeTri = function (x1, y1, x2, y2, x3, y3, cv) {
+  cv.beginPath();
+  cv.moveTo(x1,y1);
+  cv.lineTo(x2,y2);
+  cv.lineTo(x3,y3);
+  cv.lineTo(x1,y1);
+  cv.stroke();
+};
+var erase = function (x,y,width,hieght,cv) {
+  cv.clearRect(x,y,width,hieght);
+}
