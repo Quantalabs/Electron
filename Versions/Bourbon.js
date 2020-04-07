b03fee5e0c64273b9216f81214ab2a89ae3ceb51
Quanta Labs <55121845+Quantalabs@users.noreply.github.com>
2020-04-06 20:38:30 -0700

    Update Bourbon.js

--- Versions/Bourbon.js
+++ Versions/Bourbon.js
@@ -1,4 +1,3 @@
-
     var line = function (x, y, x2, y2, cv) {
       cv.beginPath();
       cv.moveTo(x,y);
@@ -13,7 +12,7 @@
       cv.lineTo(x1,y1);
       cv.fill();
     };
-    var strokeTri = function (x1, y1, x2, y2, x3, y3, cv) {
+    var strokeTri = function (xdfdfd1, y1, x2, y2, x3, y3, cv) {
       cv.beginPath();
       cv.moveTo(x1,y1);
       cv.lineTo(x2,y2);
@@ -24,84 +23,20 @@
     var erase = function (x,y,width,hieght,cv) {
       cv.clearRect(x,y,width,hieght);
     }
-    var pyramid = function (x, y, height, width, cv) {
-      cv.beginPath();
-      cv.moveTo(x,y);
-      cv.lineTo(x-(width/2),y+height);
-      cv.lineTo(x+(width/2),y+height);
-      cv.lineTo(x,y);
-      cv.fill();
-      cv.beginPath();
-      cv.moveTo(x-(width/2),y+height);
-      cv.lineTo(x-(width/2)+6,y+height-35);
-      cv.lineTo(x,y);
-      cv.fill();
-      cv.beginPath();
-      cv.moveTo(x+width/2,y+height);
-      cv.lineTo(x+width/2-6,y+height-35);
-      cv.lineTo(x,y);
-      cv.fill();
+    var shape = function (x, y, width, sides, cv) {
+        var a = sides/3;
+        var b = a * 3;
+        var c = b/sides;
+        cv.beginPath();
+        cv.moveTo(x,y);
+        cv.lineTo(x + width,y);
+        for(var i = 0; i < sides; i++) {
+            cv.lineTo(x + width*(i+1),c)
+        }
+        cv.fill();
     }
-    var cube = function (x,y, width, cv) {
-      cv.beginPath();
-      cv.moveTo(x,y);
-      cv.lineTo(x+width,y);
-      cv.lineTo(x+width-5,y-width/2);
-      cv.lineTo(x+5,y-width/2);
-      cv.lineTo(x,y);
-      cv.fill();
-      cv.beginPath();
-      cv.moveTo(x,y);
-      cv.lineTo(x+width,y);
-      cv.lineTo(x+width-5,y-width/2);
-      cv.lineTo(x+5,y-width/2);
-      cv.lineTo(x,y);
-      cv.stroke();
-      cv.beginPath();
-      cv.moveTo(x,y);
-      cv.lineTo(x,y+width);
-      cv.lineTo(x+width,y+width);
-      cv.lineTo(x+width,y);
-      cv.lineTo(x,y);
-      cv.fill();
-      cv.beginPath();
-      cv.moveTo(x,y);
-      cv.lineTo(x,y+width);
-      cv.lineTo(x+width,y+width);
-      cv.lineTo(x+width,y);
-      cv.lineTo(x,y);
-      cv.stroke();
-      cv.beginPath();
-      cv.moveTo(x,y);
-      cv.lineTo(x+5,y-width/2);
-      cv.lineTo(x+5,y+10);
-      cv.lineTo(x,y+width);
-      cv.lineTo(x,y);
-      cv.fill();
-      cv.moveTo(x,y);
-      cv.lineTo(x+5,y-width/2);
-      cv.lineTo(x+5,y+10);
-      cv.lineTo(x,y+width);
-      cv.lineTo(x,y);
-      cv.stroke();
-      cv.beginPath();
-      cv.moveTo(x+width,y);
-      cv.lineTo(x+width-5,y-width/2);
-      cv.lineTo(x+width-5,y+10);
-      cv.lineTo(x+width,y+width);
-      cv.lineTo(x+width,y);
-      cv.fill();
-      cv.beginPath();
-      cv.moveTo(x+width,y);
-      cv.lineTo(x+width-5,y-width/2);
-      cv.lineTo(x+width-5,y+10);
-      cv.lineTo(x+width,y+width);
-      cv.lineTo(x+width,y);
-      cv.fill();
-      cv.beginPath();
-      cv.moveTo(x+width-5,y+10);
-      cv.lineTo(x+5,y+10);
-      cv.stroke();
+    var cube = function (x, y, width,cv) {
+
     }
     var gradient = function (color1,color2,x,y,width,hieght,cv) {
         var grd = ctx.createLinearGradient(x,y,x+width,y+hieght);
@@ -116,11 +51,16 @@
         cv.shadowOffsetY = offsetY;
     }
     var fillText = function (x,y, text,size,cv) {
-    cv.font = size;
-    cv.strokeText(text, x, y); 
+        cv.font = size;
+        cv.strokeText(text, x, y); 
     }
-    var ellipse = function (x,y,r,cv) {
-        cv.beginPath();
-        cv.arc(x, y, r, 0, 2 * Math.PI());
-        cv.fill();
+    var fillEllipse = function (x,y,r,cv) {
+      cv.beginPath();
+      cv.arc(x,y,r,0,2*Math.PI);
+      cv.fill();
+    }
+    var strokeEllipse = function (x,y,r,cv) {
+      cv.beginPath();
+      cv.arc(x,y,r,0,2);
+      cv.stroke();
     }
