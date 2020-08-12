6b91ad1e862877877ae527b0c1ba5a89582d72d1
QLabs <55121845+Quantalabs@users.noreply.github.com>
2020-08-11 17:47:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ var shadow = function (color,blur,offsetX,offsetY,cv) {
     cv.shadowOffsetX = offsetX;
     cv.shadowOffsetY = offsetY;
 }
-var fillText = function (x,y, text,size,font,cv) {
-    cv.font = size font;
+var fillText = function (x,y, text,font,cv) {
+    cv.font = font;
     cv.strokeText(text, x, y); 
 }
 var fillEllipse = function (x,y,r,cv) {
