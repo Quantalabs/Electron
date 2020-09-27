ff1368f0294da6d4b1e6dce2aace02dd897c5b88
QLabs <55121845+Quantalabs@users.noreply.github.com>
2020-09-26 17:29:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,75 @@
-var line = function (x, y, x2, y2, cv) {
-  cv.beginPath();
-  cv.moveTo(x,y);
-  cv.lineTo(x2,y2);
-  cv.stroke();
+var c = document.createElement("canvas")
+c.id = "c"
+c.width = width
+c.height = height
+c.appendChild()
+
+var line = function (x, y, x2, y2) {
+  c.beginPath();
+  c.moveTo(x,y);
+  c.lineTo(x2,y2);
+  c.stroke();
 }
-var fillTri = function (x1, y1, x2, y2, x3, y3, cv) {
-  cv.beginPath();
-  cv.moveTo(x1,y1);
-  cv.lineTo(x2,y2);
-  cv.lineTo(x3,y3);
-  cv.lineTo(x1,y1);
-  cv.fill();
+var fillTri = function (x1, y1, x2, y2, x3, y3) {
+  c.beginPath();
+  c.moveTo(x1,y1);
+  c.lineTo(x2,y2);
+  c.lineTo(x3,y3);
+  c.lineTo(x1,y1);
+  c.fill();
 };
-var strokeTri = function (x1, y1, x2, y2, x3, y3, cv) {
-  cv.beginPath();
-  cv.moveTo(x1,y1);
-  cv.lineTo(x2,y2);
-  cv.lineTo(x3,y3);
-  cv.lineTo(x1,y1);
-  cv.stroke();
+var strokeTri = function (x1, y1, x2, y2, x3, y3) {
+  c.beginPath();
+  c.moveTo(x1,y1);
+  c.lineTo(x2,y2);
+  c.lineTo(x3,y3);
+  c.lineTo(x1,y1);
+  c.stroke();
 };
-var erase = function (x,y,width,hieght,cv) {
-  cv.clearRect(x,y,width,hieght);
+var erase = function (x,y,width,hieght) {
+  c.clearRect(x,y,width,hieght);
 }
-var pyramid = function (x, y, height, width, cv) {
-  cv.beginPath();
-  cv.moveTo(x,y);
-  cv.lineTo(x-(width/2),y+height);
-  cv.lineTo(x+(width/2),y+height);
-  cv.lineTo(x,y);
-  cv.fill();
-  cv.beginPath();
-  cv.moveTo(x-(width/2),y+height);
-  cv.lineTo(x-(width/2)-20,y+height-5);
-  cv.lineTo(x,y);
-  cv.fill();
-  cv.beginPath();
-  cv.moveTo(x+width/2,y+height);
-  cv.lineTo(x+width/2+20,y+height-5);
-  cv.lineTo(x,y);
-  cv.fill();
+var pyramid = function (x, y, height, width) {
+  c.beginPath();
+  c.moveTo(x,y);
+  c.lineTo(x-(width/2),y+height);
+  c.lineTo(x+(width/2),y+height);
+  c.lineTo(x,y);
+  c.fill();
+  c.beginPath();
+  c.moveTo(x-(width/2),y+height);
+  c.lineTo(x-(width/2)-20,y+height-5);
+  c.lineTo(x,y);
+  c.fill();
+  c.beginPath();
+  c.moveTo(x+width/2,y+height);
+  c.lineTo(x+width/2+20,y+height-5);
+  c.lineTo(x,y);
+  c.fill();
 
 }
-var gradient = function (color1,color2,x,y,width,hieght,cv) {
+var gradient = function (color1,color2,x,y,width,hieght) {
     var grd = ctx.createLinearGradient(x,y,x+width,y+hieght);
     grd.addColorStop(0, color1);
     grd.addColorStop(1, color2);
-    cv.fillStyle = grd;
+    c.fillStyle = grd;
 }
-var shadow = function (color,blur,offsetX,offsetY,cv) {
-    cv.shadowColor = color;
-    cv.shadowBlur = blur;
-    cv.shadowOffsetX = offsetX;
-    cv.shadowOffsetY = offsetY;
+var shadow = function (color,blur,offsetX,offsetY) {
+    c.shadowColor = color;
+    c.shadowBlur = blur;
+    c.shadowOffsetX = offsetX;
+    c.shadowOffsetY = offsetY;
 }
-var text = function (x, y, text, cv) {
-  cv.strokeText(text, x, y);
+var text = function (x, y, text) {
+  c.strokeText(text, x, y);
 }
-var fillEllipse = function (x,y,r,cv) {
-  cv.beginPath();
-  cv.arc(x,y,r,0,2*Math.PI);
-  cv.fill();
+var fillEllipse = function (x,y,r) {
+  c.beginPath();
+  c.arc(x,y,r,0,2*Math.PI);
+  c.fill();
 }
-var strokeEllipse = function (x,y,r,cv) {
-  cv.beginPath();
-  cv.arc(x,y,r,0,2);
-  cv.stroke();
+var strokeEllipse = function (x,y,r) {
+  c.beginPath();
+  c.arc(x,y,r,0,2);
+  c.stroke();
 }
